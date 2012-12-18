f8f542654343c38fdd4441505e833884150a8fdb
kurtcc <kurtcc@github.com>
2012-12-18 11:07:15 +0200

    Added function to include stdin

--- hasgps.go
+++ hasgps.go
@@ -2,13 +2,21 @@ package GoExifGPS
 
 import (
 	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/tiff"
+	"io"
 	"log"
 	"os"
-	"io"
 )
 
+type Exif struct {
+	tif *tiff.Tiff
+
+	main map[FieldName]*tiff.Tag
+}
+type FieldName string
+
 func ContainsGPS(fname string) bool {
-	
+
 	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
@@ -17,13 +25,22 @@ func ContainsGPS(fname string) bool {
 	x, err := exif.Decode(f)
 	f.Close()
 	if err != nil {
-	   return false
-	   //log.Fatal(err) log.Fatal makes the program stop so we don't need this
-	   // coz we won't be able to get false ever.
+		return false
+		//log.Fatal(err) log.Fatal makes the program stop so we don't need this
+		// coz we won't be able to get false ever.
 	}
 	_, Has := x.Get("GPSLatitude")
 	return Has != io.EOF
-	
-                      
-}  
 
+}
+
+// Remember to add error checking here
+func ContainsGPSFromStdin() bool {
+	r := os.Stdin
+	x, err := exif.Decode(f)
+	if err != nil {
+		return false
+	}
+	_, Has := x.Get("GPSLatitude")
+	return Has != io.EOF
+}
