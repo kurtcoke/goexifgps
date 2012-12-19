928a00f4ae051ed565b6c02b7a72b1f8baccbf78
kurtcc <kurtcc@github.com>
2012-12-19 14:20:29 +0200

    Library that does Parse and Format just all in one line.

---
+++ DecodeGPS.go
@@ -0,0 +1,26 @@
+package GoExifGPS
+
+import (
+	"fmt"
+	"github.com/kurtcc/GoExifGPS"
+)
+/* Assume we know the file has gps exif data. Use hasgps.go to check for it, has two functions
+ for doing that.  We don't want to run through this function here completely only to realise we did all that processing just to get an error "Does not contain gps data. */
+func DecodeGPS(filename string) (string, error) {
+    var Location string 
+	PointToPic, err := GoExifGPS.OpenClose(filename)
+	if err != nil {
+		return Location, err
+	}
+	if err == nil {
+		LatRef, Lat , LongRef, LongD := GoExifGPS.OpenParseJson(PointToPic)
+		Latitude := GoExifGPS.FormatGPS(Lat)
+		Longitude :=  GoExifGPS.FormatGPS(LongD)
+		Latitude = GoExifGPS.RefFormat(LatRef, Latitude)
+        Longitude =  GoExifGPS.RefFormat(LongRef,Longitude)
+		Location = GoExifGPS.MapFriendly(Latitude, Longitude)
+		return Location, err
+	}
+	return Location, err
+}
+
