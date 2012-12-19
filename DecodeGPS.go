ca4c722ec1ae864d437e9e62c257b54f23640756
kurtcc <kurtcc@github.com>
2012-12-19 15:33:42 +0200

    Added something more.

--- DecodeGPS.go
+++ DecodeGPS.go
@@ -1,6 +1,5 @@
 package GoExifGPS
 
-import "github.com/kurtcc/GoExifGPS"
 /* Assume we know the file has gps exif data. Use hasgps.go to check for it, has two functions
 for doing that.  We don't want to run through this function here completely only to realise we did all that processing just to get an error "Does not contain gps data. */
 func DecodeGPS(filename string) (string, error) {
