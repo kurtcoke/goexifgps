d89724033c9ba28fecc2e1ab0a232e1452302f8b
kurtcc <kurtcc@github.com>
2012-12-20 15:08:11 +0200

    Made big changes, testdata added earlier

--- GoExifGPS_test.go
+++ GoExifGPS_test.go
@@ -2,13 +2,15 @@ package GoExifGPS
 
 import "testing"
 
-func TestContainsGPS( t *testing.T) {
+func TestContainsGPS(t *testing.T) {
 	TrueImage := "testdata/_JEF018993_sm.jpg"
-	if ContainsGPS(TrueImage) != true {
+	YesGPS, err := ContainsGPS(TrueImage)
+	if YesGPS != true {
 		t.Error("Image has GPS data, this should work fine!")
 	}
-  FalseImage := "testdata/WTgX4.jpg"
-  if ContainsGPS(FalseImage) == true {
-	  t.Error("Image does not contain gps data!")
-  }
+	FalseImage := "testdata/WTgX4.jpg"
+	NoGPS, err2 := ContainsGPS(FalseImage)
+	if NoGPS == true {
+		t.Error("Image does not contain gps data!")
+	}
 }
--- Hasgps.go
+++ Hasgps.go
@@ -3,36 +3,32 @@ package GoExifGPS
 import (
 	"github.com/rwcarlsen/goexif/exif"
 	"io"
-	"log"
 	"os"
 )
 
-func ContainsGPS(fname string) bool {
-
+// I need this: especially for walking folders.
+func ContainsGPS(fname string) (bool, error) {
 	f, err := os.Open(fname)
 	if err != nil {
-		log.Fatal(err)
+		return false, err
 	}
-
-	x, err := exif.Decode(f)
-	f.Close()
-	if err != nil {
-		return false
-		//log.Fatal(err) log.Fatal makes the program stop so we don't need this
-		// coz we won't be able to get false ever.
-	}
-	_, Has := x.Get("GPSLatitude")
-	return Has != io.EOF
+	Contains, err2 := ContainsGPSFromStdin(f)
+	return Contains, err
+	// Ok fine this works , later make ContainsGPSFromStdin return errors too
 
 }
 
 // Remember to add error checking here
-func ContainsGPSFromStdin() bool {
-	r := os.Stdin
-	x, err := exif.Decode(r)
+func ContainsGPSFromStdin(R io.Reader) (bool, error) {
+	x, err := exif.Decode(R)
 	if err != nil {
-		return false
+		return false, err
 	}
-	_, Has := x.Get("GPSLatitude")
-	return Has != io.EOF
+	_,err2 := x.Get("GPSLatitude")   //returns (*tiff.Tag, error)
+	if err2 == nil {
+		return true, nil
+	}
+	if err2 != nil {
+		return false, err2
+}                                                            
 }
