bac7cc852a57a53c12b803db8a2d227f478ab772
kurtcc <kurtcc@github.com>
2012-12-21 14:01:47 +0200

    Made changes, less code is better.

--- GoExifGPS_test.go
+++ GoExifGPS_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestContainsGPS(t *testing.T) {
 	TrueImage := "testdata/_JEF018993_sm.jpg"
-	YesGPS,_ := ContainsGPS(TrueImage)
+	YesGPS, _ := ContainsGPS(TrueImage)
 	if YesGPS != true {
 		t.Error("Image has GPS data, this should work fine!")
 	}
-	/* FalseImage := "testdata/WTgX4.jpg"
+	FalseImage := "testdata/WTgX4.jpg"
 	NoGPS, _ := ContainsGPS(FalseImage)
 	if NoGPS == true {
 		t.Error("Image does not contain gps data!")
-	}  */
+	}
 }
