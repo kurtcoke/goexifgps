89ba5b85b638bff0f69e30cf03501ff9eaa2ac76
kurtcc <kurtcc@github.com>
2012-12-21 15:21:34 +0200

    should work fine now

---
+++ GoExifGPS2_test.go
@@ -0,0 +1,12 @@
+package GoExifGPS
+
+import "testing"
+
+func TestOpenClose(t *testing.T) {
+	TrueImage := "testdata/_JEF018993_sm.jpg"
+	// This image is working and should open with no errors.
+	_, err := OpenClose(TrueImage)
+	if err != nil {
+		t.Error("This image has been tested and should work.", err) //Should I put err here as well?
+	}
+}
