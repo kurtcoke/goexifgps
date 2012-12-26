709df61b635b8d63b1228ec2e45b9da79f1bb56a
kurtcc <kurtcc@github.com>
2012-12-27 01:10:20 +0200

    test works

--- GoExifGPS3_test.go
+++ GoExifGPS3_test.go
@@ -2,4 +2,17 @@ package GoExifGPS
 
 import "testing"
 
+func TestDecodeGPS(t *testing.T) {
+	TrueImage := "testdata/_JEF018993_sm.jpg"
+	//Image does contain GPS data.
+	//FalseImage := "testdata/WTgX4.jpg"
+	// Does not contain GPS data.
+	// First will return string, latter will return error.
+	ExifData, _ := OpenClose(TrueImage)
+	// We know the above works, we have GoExifGPS2_test.go to prove it.
+	GPSLocation := DecodeGPS(ExifData)
+	if len(GPSLocation) == 0  {
+		t.Error("This image should open fine with no errors!")
+	}
+}
 
