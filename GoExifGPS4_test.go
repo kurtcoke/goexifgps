49744a11ffc5f747a9459951275f458b2dd878cb
kurtcc <kurtcc@github.com>
2012-12-29 11:16:17 +0200

    still needs some changes.

--- GoExifGPS4_test.go
+++ GoExifGPS4_test.go
@@ -7,4 +7,9 @@ func TestRefFormat( t *testing.T) {
 	// if ref is "N" or "E" then decGPS stays the same
 	// but if ref is Anything else then decGPS becomes negative.
 	// decGPS becomes negative ( decGPS is multiplied by -1 of float32 type)
+    DecGPS = float32(1.0)  
+    
+	Nref = "N"
+	Noref = ''  // try this with nil as decGPS float32 value.
+	Wref = "W" // We know what this will do
 
