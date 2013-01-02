d16ed7e4fbc7915c090eb03a71a0ea3bcadb3b93
kurtcc <kurtcc@github.com>
2013-01-02 15:34:40 +0200

    Has gps needed shorter function names, this is not Java.

--- Hasgps.go
+++ Hasgps.go
@@ -7,25 +7,25 @@ import (
 )
 
 // I need this: especially for walking folders.
-func ContainsGPS(fname string) (bool, error) {
+func Contains(fname string) (bool, error) {
 	f, err := os.Open(fname)
 	if err != nil {
 		return false, err
 	}
 	var Contains bool
-	Contains, err = ContainsGPSFromStdin(f)
+	Contains, err = ContainStdin(f)
 	/* if err2 == nil {
 	return Contains, nil
 	} else {
 		return false ,err2
 	}
-	// Ok fine this works , later make ContainsGPSFromStdin return errors too
+	// Ok fine this works , later make ContainStdin return errors too
 	*/
 	return Contains, err
 }
 
 // Remember to add error checking here
-func ContainsGPSFromStdin(R io.Reader) (bool, error) {
+func ContainStdin(R io.Reader) (bool, error) {
 
 	x, err := exif.Decode(R)
 	if err != nil {
