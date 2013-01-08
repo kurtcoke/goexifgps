5f481f607d80116f80402ca8c88542df000916d3
kurtcc <kurtcc@github.com>
2013-01-08 14:17:27 +0200

    this should work fine

--- decodegps.go
+++ decodegps.go
@@ -16,15 +16,15 @@ Location , err2 := GoExifGPS.DecodeGPS(ExifData)
 
 /* Assume we know the file has gps exif data. Use hasgps.go to check for it, has two functions
 for doing that.  We don't want to run through this function here completely only to realise we did all that processing just to get an error "Does not contain gps data. */
-
+                         //  LatRef, Lat, LongRef, LongD
 func DecodeGPS(ExifData *exif.Exif) string {
 	// (string,error)
-	LatRef, Lat, LongRef, LongD := GetGPS(ExifData)
+	F := GetGPS(ExifData)
 	// Need OpenParseJson to catch errors so we can add error checking here.
-	Latitude := FormatGPS(Lat)
-	Longitude := FormatGPS(LongD)
-	Latitude = RefFormat(LatRef, Latitude)
-	Longitude = RefFormat(LongRef, Longitude)
+	Latitude := FormatGPS(F.Lat)
+	Longitude := FormatGPS(F.LongD)
+	Latitude = RefFormat(F.LatRef, Latitude)
+	Longitude = RefFormat(F.LongRef, Longitude)
 	Location := MapFriendly(Latitude, Longitude)
 	return Location
 	// Maybe later make OpenParseJson return errors if/when they occur.
