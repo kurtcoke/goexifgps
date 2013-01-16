40aabd82a5b2ac65e8c99242f56fa73b9db84124
kurtcc <kurtcc@github.com>
2013-01-16 10:30:38 +0200

    added another test case, should work fine.

--- goexifgps_test.go
+++ goexifgps_test.go
@@ -5,8 +5,16 @@ import "testing"
 func TestOpenClose(t *testing.T) {
 	TrueImage := "testdata/_JEF018993_sm.jpg"
 	// This image is working and should open with no errors.
-	_, err := OpenClose(TrueImage)
+	ExifData, err := OpenClose(TrueImage)
+	Location, err := DecodeGPS(ExifData)
 	if err != nil {
 		t.Error("This image has been tested and should work.", err)
+	}
+	FalseImage := "testdata/WTgX4.jpg"
+	ExifData2, err := OpenClose(FalseImage)
+	//This should work fine still, but below should return error.
+	_, err := DecodeGPS(ExifData2)
+	if err == nil {
+		t.Error("This image has been tested and should return an error!", err)
 	}
 }
