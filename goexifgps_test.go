dacb64d371e63f673abbe15ad1ab76b00806ff0b
kurtcc <kurtcc@github.com>
2013-01-16 12:30:00 +0200

    removed some code

--- goexifgps_test.go
+++ goexifgps_test.go
@@ -10,11 +10,5 @@ func TestOpenClose(t *testing.T) {
 	if err != nil {
 		t.Error("This image has been tested and should work.", err)
 	}
-	FalseImage := "testdata/WTgX4.jpg"
-	ExifData2, err := OpenClose(FalseImage)
-	//This should work fine still, but below should return error.
-	_, err = DecodeGPS(ExifData2)
-	if err == nil {
-		t.Error("This image has been tested and should return an error!", err)
-	}
+  
 }
