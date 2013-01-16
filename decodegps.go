46be722d69572faafbea81ed2916647918c6ef41
kurtcc <kurtcc@github.com>
2013-01-16 12:17:26 +0200

    change

--- decodegps.go
+++ decodegps.go
@@ -4,19 +4,7 @@ import "github.com/rwcarlsen/goexif/exif"
 
 // Author kurtcc on github.com
 
-/*
-ExifData , err := exif.Decode(r io.Reader) Takes an io.Reader
-Location, err := GoExifGPS.DecodeGPS(ExifData)
 
-Or 
-
-ExifData, err := OpenClose(filename)
-Location , err2 := GoExifGPS.DecodeGPS(ExifData)
-*/
-
-/* Assume we know the file has gps exif data. Use hasgps.go to check for it, has two functions
-for doing that.  We don't want to run through this function here completely only to realise we did all that processing just to get an error "Does not contain gps data. */
-//  LatRef, Lat, LongRef, LongD
 func DecodeGPS(ExifData *exif.Exif) (string, error) {
 	// (string,error)
 	F, err := GetGPS(ExifData)
