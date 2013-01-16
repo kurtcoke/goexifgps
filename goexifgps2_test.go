95304247411bd8f0d931ccf8882d0a5b0bfd8b5c
kurtcc <kurtcc@github.com>
2013-01-16 12:11:57 +0200

    No tests for functions that dont return errors

--- goexifgps2_test.go
+++ goexifgps2_test.go
@@ -2,21 +2,15 @@ package goexifgps
 
 import "testing"
 
-//So RefFormat is in Format.go
-func TestRefFormat(t *testing.T) {
-	// RefFormat takes (ref string, decGPS float32)
-	// if ref is "N" or "E" then decGPS stays the same
-	// but if ref is Anything else then decGPS becomes negative.
-	// decGPS becomes negative ( decGPS is multiplied by -1 of float32 type)
-	DecGPS := float32(1.0)
-
-	Nref := "N"
-	//Noref = ''  // try this with nil as decGPS float32 value.
-	//Wref = "W" // We know what this will do
-
-	RefFormResult := RefFormat(Nref, DecGPS)
-
-	if RefFormResult != DecGPS {
-		t.Error("The result should be unchanged, should stay positive.")
+func TestDecodeGPS(t *testing.T) {
+	TrueImage := "testdata/_JEF018993_sm.jpg"
+	//Image does contain GPS data.
+	//FalseImage := "testdata/WTgX4.jpg"
+	// Does not contain GPS data.
+	// First will return string, latter will return error.
+	ExifData, err := OpenClose(TrueImage)
+	// We know the above works, we have GoExifGPS2_test.go to prove it.
+	if err != nil {
+		t.Error("This should work without returning an error.")
 	}
 }
