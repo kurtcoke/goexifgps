91dc52ddd992355ba6685e0cef5d4b937f44c538
kurtcc <kurtcc@github.com>
2013-01-16 10:48:45 +0200

    made some changes

--- goexifgps2_test.go
+++ goexifgps2_test.go
@@ -3,17 +3,15 @@ package goexifgps
 import "testing"
 
 func TestDecodeGPS(t *testing.T) {
-	TrueImage := "testdata/_JEF018993_sm.jpg"
+	//TrueImage := "testdata/_JEF018993_sm.jpg"
 	//Image does contain GPS data.
-	//FalseImage := "testdata/WTgX4.jpg"
+	FalseImage := "testdata/WTgX4.jpg"
 	// Does not contain GPS data.
-	// First will return string, latter will return error.
-	ExifData, _ := OpenClose(TrueImage)
-	// We know the above works, we have GoExifGPS2_test.go to prove it.
-	GPSLocation := DecodeGPS(ExifData)
-	if len(GPSLocation) == 0 {
-		t.Error("This image should open fine with no errors!")
-		// Should return a string value that can be used to find location on 
-		// google maps
+	// Should return an error since exif.Get("GPSLatitude") will return an error if no
+	// data for given field is available.
+	 _ , err:= OpenClose(FalseImage)
+	if err == nil {
+		t.Error("This image should return an error!")
+		
 	}
 }
