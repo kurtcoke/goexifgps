13d4f9d694fa36e3657ffdfeec76722dc5f1acee
kurtcc <kurtcc@github.com>
2013-01-16 12:25:22 +0200

    correction

--- goexifgps_test.go
+++ goexifgps_test.go
@@ -6,7 +6,7 @@ func TestOpenClose(t *testing.T) {
 	TrueImage := "testdata/_JEF018993_sm.jpg"
 	// This image is working and should open with no errors.
 	ExifData, err := OpenClose(TrueImage)
-	_, err := DecodeGPS(ExifData)
+	_, err = DecodeGPS(ExifData)
 	if err != nil {
 		t.Error("This image has been tested and should work.", err)
 	}
