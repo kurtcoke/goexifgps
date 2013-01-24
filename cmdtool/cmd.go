fe642aa7f12653bba4597ddb7d329e887dab5569
kurtcc <kurtcc@github.com>
2013-01-24 15:51:13 +0200

    go fmt

--- cmdtool/cmd.go
+++ cmdtool/cmd.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "github.com/rwcarlsen/goexif/exif"
-    "github.com/kurtcc/goexifgps"
-    "io"
-    "os"
+	"fmt"
+	"github.com/kurtcc/goexifgps"
+	"github.com/rwcarlsen/goexif/exif"
+	"io"
+	"os"
 )
 
 func main() {
-    var R io.Reader
-    R = os.Stdin
-    ExifData, err := exif.Decode(R)
-    if err == io.EOF {
-    fmt.Fprintf(os.Stderr,"%s\n","Error decoding file.") 
-    }   
-    if err != io.EOF {
-        Location, err2 := goexifgps.DecodeGPS(ExifData)
-        if err2 != io.EOF {
-            fmt.Println(Location)
-        }   
-        if err2 == io.EOF {
-        fmt.Fprintf(os.Stderr,"%s\n","Contains no GPS exif data.")
-        }   
-    }   
+	var R io.Reader
+	R = os.Stdin
+	ExifData, err := exif.Decode(R)
+	if err == io.EOF {
+		fmt.Fprintf(os.Stderr, "%s\n", "Error decoding file.")
+	}
+	if err != io.EOF {
+		Location, err2 := goexifgps.DecodeGPS(ExifData)
+		if err2 != io.EOF {
+			fmt.Println(Location)
+		}
+		if err2 == io.EOF {
+			fmt.Fprintf(os.Stderr, "%s\n", "Contains no GPS exif data.")
+		}
+	}
 
 }
-
