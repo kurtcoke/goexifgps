e13cc82ef8a4d74e1f9cd14e95fee2f88300da7f
kurtcc <kurtcc@github.com>
2013-01-24 15:37:18 +0200

    go fmt

--- goexifgps3_test.go
+++ goexifgps3_test.go
@@ -4,18 +4,15 @@ import "testing"
 
 func TestGetGPS(t *testing.T) {
 	TrueImage := "testdata/_JEF018993_sm.jpg"
-	// Check that TrueImage returns no error
+	// Check that TrueImage returns no errors
 
 	TrueI, err := OpenClose(TrueImage)
-	// error checking
 	if err != nil {
 		t.Error("This image should open and decode fine.")
 	}
-	
 	_, err = GetGPS(TrueI)
 	// If err != nil something is wrong
 	if err != nil {
 		t.Error("This image should work without returning errors!")
 	}
-	
 }
