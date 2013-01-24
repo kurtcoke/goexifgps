4266af94bacfbdff0ef34619bad26569ac47a1a3
kurtcc <kurtcc@github.com>
2013-01-24 16:06:33 +0200

    added some comments

--- format.go
+++ format.go
@@ -5,23 +5,24 @@ import (
 	"fmt"
 	"github.com/rwcarlsen/goexif/tiff"
 )
-
+// Get values written as big.Rat returns the result in a more sensible formatting.
 func FormatGPS(t *tiff.Tag) float32 {
 
 	Dec := make([]float32, 3)
 	for count := 0; count < 3; count++ {
 
-		Numer, Denom := t.Rat2(count) // They are now of int64 type
+		Numer, Denom := t.Rat2(count) 
 		Dec[count] = float32(Numer) / float32(Denom)
 	}
 
 	DecGPS := float32(Dec[0]) + float32(Dec[1]/60) + float32(Dec[2]/3600)
 	return DecGPS
-	//dGPS := Hours + Minutes/float32(60) + Seconds/float32(3600)
+	//DecGPS := Hours + Minutes/float32(60) + Seconds/float32(3600)
 }
-
-func RefFormat(ref string, decGPS float32) float32 { //Pass Ref and result of formatGPS
-	// The program would throw errors if there was no gps data before it ran this part!
+//Pass it Lat or Long Ref values and result of formatGPS    
+// For Latitude: If N the result is positive if S then negative.
+// For Longitude: If the result is E then it is positive, if it is W then it is negative.
+func RefFormat(ref string, decGPS float32) float32 { 
 	switch ref {
 	case "N":
 		break
@@ -34,10 +35,11 @@ func RefFormat(ref string, decGPS float32) float32 { //Pass Ref and result of fo
 	}
 	return decGPS
 }
-func MapFriendly(latt, lonn float32) string {
-	//Takes values of latitude and longitude returned by RefFormat
+ 	//Takes values of latitude and longitude returned by RefFormat
 	// and turns into a string that is in decimal and can be used on google maps
 	// and thus is "map friendly"     
+                      
+func MapFriendly(latt, lonn float32) string {
 
 	return fmt.Sprintf("%v,%v", latt, lonn)
 }
